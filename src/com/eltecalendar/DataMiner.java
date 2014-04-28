7601d18d426d0500d983ffcfaced33db14a1e9fa
z0grim <zodflakmonkey@freemail.hu>
2014-04-28 12:43:19 +0200

    DataMiner initial commit (test)
    (project.properties nálam nem fogadta el az android-13-at
    user interfészen kiválasztott aktuális android verzió felrakta 19-re
    ezt nem kommitoltam fel)

--- src/com/eltecalendar/DataMiner.java
+++ src/com/eltecalendar/DataMiner.java
@@ -2,4 +2,22 @@ package com.eltecalendar;
 
 public class DataMiner {
 
+	DatabaseHelper db;
+	
+	public DataMiner(DatabaseHelper dbh) {
+		db=dbh;
+	}
+	
+	public void getNewDatabase() {
+		//db.deleteDatabase();
+		//db.createEmptyDatabase();
+		
+		//db.addCourse();
+	}
+	
+	public void updateDatabase() {
+		
+		//db.updateCourse();
+		
+	}
 }
